a2bfdf5535d8a9e41aa57c875fc58a4b73bd9ed0
Alexr16 <j.alex-1996@hotmail.com>
2022-04-05 14:06:36 -0500

    Implement add score function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.scss';
+import { add, display } from './modules/functions.js';
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  add();
+  display();
+});
