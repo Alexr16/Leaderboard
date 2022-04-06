3e4aac93c19ac4d5eaa57e83dd26c1d8ae3c4931
Alexr16 <j.alex-1996@hotmail.com>
2022-04-06 13:58:53 -0500

    Create LoaderBoard API key

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,22 +1,28 @@
-const scores = [];
-const display = (name, score) => {
-  const scoreList = document.getElementById('score-list');
-  const scoreContainer = document.createElement('div');
-  scoreContainer.classList.add('scores-container');
-  scoreContainer.innerHTML = `
-        <p>${name}: ${score}</p>
-    `;
-  scoreList.appendChild(scoreContainer);
+import { getUsers, createUser } from './api.js';
+
+const scoreList = document.getElementById('score-list');
+
+const display = async () => {
+  const data = await getUsers();
+  while (scoreList.firstChild) {
+    scoreList.removeChild(scoreList.firstChild);
+  }
+  data.result.forEach((entry) => scoreList.insertAdjacentHTML('beforeend', `
+          <div class="scores-container">
+            <p>${entry.user}: ${entry.score}</p>
+          </div>
+      `));
 };
 
 const add = () => {
   const nameInput = document.getElementById('name-form');
   const scoreInput = document.getElementById('score-form');
-  if (nameInput.value) {
-    const name = nameInput.value;
-    const score = scoreInput.value;
-    scores.push({ name, score });
-    display(name, score);
+  if (nameInput.value !== '' && scoreInput.value !== '') {
+    const data = {
+      user: nameInput.value,
+      score: scoreInput.value,
+    };
+    createUser(data);
     nameInput.value = null;
     scoreInput.value = null;
   }
